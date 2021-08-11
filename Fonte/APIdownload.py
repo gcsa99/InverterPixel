754e10453cb690333e0c6692f5ac4086681fa791
gcsa99 <gui.lusa.99@gmail.com>
2021-08-11 17:09:06 -0300

    funcionando com busca automatizada no google

--- Fonte/APIdownload.py
+++ Fonte/APIdownload.py
@@ -1,7 +1,17 @@
 #Imports Packages
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-#Opens up web driver and goes to Google Images
-driver = webdriver.Edge('C:/msedgedriver.exe')
-driver.get('https://www.google.com')
+from selenium.webdriver.common.keys import Keys 
+
+def carregarImagem(keyword):
+    #Opens up web driver and goes to Google Images
+    driver = webdriver.Edge('C:/msedgedriver.exe')
+    driver.get('https://www.google.com.br/imghp?hl=pt-BR&ogbl')
+
+    box = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
+
+    box.send_keys(keyword)
+    box.send_keys(Keys.ENTER)
+    imagem = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img')
+    imagem.click()
+    driver.find_element_by_xpath('//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img').screenshot('images/'+keyword+'.jpg')
+    return 'images/'+keyword+'.jpg'
--- Fonte/inverterpixel.py
+++ Fonte/inverterpixel.py
@@ -3,11 +3,14 @@ import numpy as np
 from PIL import Image  
 from numpy import asarray
 
+from APIdownload import carregarImagem
+
+pesquisa=input()
 # Carregar imagem original
 #image = Image.open('images/mascote.jpg')
 #image = Image.open('images/meme.jpg')
 #image = Image.open('images/sapo.jpg')
-image = Image.open('images/ifsp.jpg')
+image = Image.open(carregarImagem(pesquisa))
 
 #informações imagem original
 #tipo
@@ -17,7 +20,7 @@ print(image.size)
 #modo (RGB, L, 1, etc)
 print(image.mode)
 # quantidade de bits por pixel
-print(image.bits)
+#batataprint(image.bits)
 # valores do maior e menor pixel
 print(image.getextrema())
 
@@ -29,13 +32,13 @@ convertedImage = image.convert('L')
 #convertedImage.save('images/mascote-convertido.jpg')
 #convertedImage.save('images/meme-convertido.jpg')
 #convertedImage.save('images/sapo-convertido.jpg')
-convertedImage.save('images/ifsp-convertido.jpg')
+convertedImage.save('images/'+pesquisa+'-convertido.jpg')
 
 #carregar imagem convertida
 #image = Image.open('images/mascote-convertido.jpg')
 #image = Image.open('images/meme-convertido.jpg')
 #image = Image.open('images/sapo-convertido.jpg')
-image = Image.open('images/ifsp-convertido.jpg')
+image = Image.open('images/'+pesquisa+'-convertido.jpg')
 
 
 #informações da imagem convertida
@@ -62,4 +65,4 @@ imagemInvertida = Image.fromarray(npImage)
 #imagemInvertida.save('images/mascote-invertido.jpg')
 #imagemInvertida.save('images/meme-invertido.jpg')
 #imagemInvertida.save('images/sapo-invertido.jpg')
-imagemInvertida.save('images/ifsp-invertido.jpg')
+imagemInvertida.save('images/'+pesquisa+'-invertido.jpg')
